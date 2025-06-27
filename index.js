e95edadda3fe78459b31fd0af036b13fbc5d5ac1
Savant <bentobeanbox@gmail.com>
2025-06-27 07:25:41 -0500

    super good progress. added local time, logo, and better styling

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
 document.addEventListener("DOMContentLoaded", () => {
-const grid = document.getElementById("grid")
+    const grid = document.getElementById("grid")
+    
+    const projects = {
+        project1: {
+            name: "Top Three Anime",
+            html: "/my-top-animes-homepage/index.html"
+        },
+        project2: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project3: {
+            name: "&nbspunknown",
+            html: ""
 
-const projects = {
-    project1: {
-        name: "Top Animes",
-        html: "/my-top-animes-homepage/index.html"
+        },
+        project4: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project5: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project6: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project7: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project8: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project9: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project10: {
+            name: "&nbspunknown",
+            html: ""
+        },
+        project11: {
+            name: "&nbspunknown",
+            html: ""
+        }
+    }
 
-    },
-    project2: "unknown",
-    project3: "unknown",
-    project4: "unknown",
-    project5: "unknown",
-    project6: "unknown",
-    project7: "unknown",
-    project8: "unknown",
-    project9: "unknown",
-    project10: "unknown",
-    project11: "unknown",
-    project12: "unknown",
-    project13: "unknown",
-    project14: "unknown",
-    project15: "unknown",
-    project16: "unknown",
-    project17: "unknown",
+    for (let project in projects) {
+        const name = projects[project].name
+        const html = projects[project].html
+        const a = document.createElement("a")
+        a.href = `${html}`
+
+        a.innerHTML = `<img class="folder-image" src="/closed-folder.png">${name}`
+
+        grid.append(a)
+    }
+
+    const time = document.getElementById('time')
+
+    function updateTime() {
+    const now = new Date()
+    const timeString = now.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+    })
+
+    console.log(timeString)
+
+    time.innerHTML = `${timeString}`
 }
 
-for (let project in projects) {
-    const name = projects[project].name
-    const html = projects[project].html
-    const a = document.createElement("a")
-    a.href = `${html}`
-
-    a.innerHTML = `<img class="folder-image" src="/closed-folder.png">${name}`
-
-    grid.append(a)
-}
+setInterval(updateTime, 1000)
 })
+
